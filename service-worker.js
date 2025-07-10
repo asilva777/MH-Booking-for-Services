049b40d4c4e517637e89becaaa3776e441877715
ASilva Innovations <106929764+asilva777@users.noreply.github.com>
2025-07-10 10:00:16 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,22 @@
-const CACHE_NAME = "mh-booking-cache-v1";
-const urlsToCache = [
+const CACHE_NAME = "cognitio-booking-v1";
+const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
-  "/styles.css",
-  "/main.js",
+  "/style.css",
+  "/script.js",
   "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
   );
 });
